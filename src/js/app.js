c28e3722d4149b2aa9626ff20e21bc3931fb3004
Kamila Tomczyk <ka1130@o2.pl>
2017-02-11 12:04:05 +0100

    Overlay menu working.

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,21 @@
 (function() {
     document.addEventListener("DOMContentLoaded", function() {
-        console.log("ok");
+
+        // Hamburger menu
+        const openBtn = document.getElementsByClassName("openbtn")[0];
+        const closeBtn = document.getElementsByClassName("closebtn")[0];
+        const nav = document.getElementsByTagName("nav")[0];
+
+        function openNav() {
+            nav.style.height = "100%";
+        }
+
+        function closeNav() {
+            nav.style.height = "0%";
+        }
+
+        openBtn.addEventListener("click", openNav, false);
+        closeBtn.addEventListener("click", closeNav, false);
+
     }, false)
 })();
