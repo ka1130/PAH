e8682786be6ddf3892235acc2176f04d861ad38f
Kamila Tomczyk <ka1130@o2.pl>
2017-03-01 17:00:27 +0100

    Add dist.

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,62 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var plumber = require('gulp-plumber');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var plumber = require("gulp-plumber");
+var browserSync = require("browser-sync");
+var del = require("del");
+var useref = require("gulp-useref");
+var uglify = require("gulp-uglify");
+var pump = require("pump");
+var imagemin = require("gulp-imagemin");
 
-gulp.task('jshint', function() {
-    return gulp.src('js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
-gulp.task('sass', function() {
-    return gulp.src('src/sass/main.scss')
-        .pipe(sourcemaps.init())
+gulp.task("sass", function() {
+    gulp.src("src/sass/main.scss")
         .pipe(plumber())
-        .pipe(sass.sync({
-            outputStyle: 'expanded',
-            errLogToConsole: true
-        }))
+        .pipe(sass.sync())
         .pipe(autoprefixer({
-            browsers: ['last 5 version', 'IE 9']
+            browsers: ["last 5 version", "IE 9"]
         }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('src/css'));
+        .pipe(gulp.dest("src/css/"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-    gulp.watch('src/sass/**/*.scss', ['sass']);
+gulp.task("server", function() {
+    browserSync.init({
+        server: "src/"
+    });
 });
+
+gulp.task("watch", function() {
+    gulp.watch("src/sass/**/*.scss", ["sass"]);
+    gulp.watch(["src/*.html", "src/**/*.js"], browserSync.reload);
+});
+
+gulp.task("clean", function() {
+    del("dist/");
+});
+
+gulp.task("minify", function() {
+    gulp.src("src/*.html")
+        .pipe(useref())
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("compress", function(cb) {
+    pump([
+            gulp.src("src/js/*.js"),
+            uglify(),
+            gulp.dest("dist")
+        ],
+        cb
+    );
+});
+
+gulp.task("img", function() {
+    gulp.src("src/img/*", {
+            base: "src/"
+        })
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("default", ["sass", "server", "watch"]);
